6feb0f69b05c5850abba2a0c769aee698ca23895
hankhsu1996 <hank850503@gmail.com>
2020-03-05 13:42:38 +0800

    Add github deployment messages

--- scripts/deploy-github.js
+++ scripts/deploy-github.js
@@ -1,13 +1,18 @@
 const ghpages = require("gh-pages");
 
 // replace with your repo url
+console.log("Started deploying to hankhsu1996.github.io");
 ghpages.publish(
     "public",
     {
         branch: "master",
         repo: "https://github.com/hankhsu1996/hankhsu1996.github.io.git"
     },
-    () => {
-        console.log("Deployment complete!");
+    err => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Finished deploying to hankhsu1996.github.io");
+        }
     }
 );
